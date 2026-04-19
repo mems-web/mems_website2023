f47da6cb87996aa8fa198fbfbb47d1ba2c40ec9c
PranavManurkar <pranavmanurkar94@gmail.com>
2026-04-19 21:59:11 +0530

    please destory this codebase and restart for the love of the almighty, bleach my eyes!!!

--- src/components/profiles/profile18.jsx
+++ src/components/profiles/profile18.jsx
@@ -37,7 +37,7 @@ const Profile18 = () => {
               <li>Email	 (<i><FaEnvelope/></i>)	: kvvamsi@iiti.ac.in </li>
               <li>Contact  (<i><FaPhone/></i>)		: +91-731-660-3333 ext: 5568</li>
             <div className="fac-links">
-             <a href="https://sites.google.com/view/kvvamsi/home " target="_blank" rel="noreferrer"> <i><FaHome/></i></a>
+             <a href="https://kvvamsi.profiles.iiti.ac.in/ " target="_blank" rel="noreferrer"> <i><FaHome/></i></a>
              <a href=" https://scholar.google.co.in/citations?user=ducGUjAAAAAJ&hl=en  "target="_blank"  rel="noreferrer"> <i><SiGooglescholar/></i></a>
              <a href="https://www.researchgate.net/profile/K-V-Vamsi "target="_blank"  rel="noreferrer"><i><FaResearchgate/></i></a>
              <a href="https://twitter.com/kvvamsi "target="_blank"  rel="noreferrer"><i><FaTwitterSquare/></i></a>
--- src/components/profiles/vamsi.jsx
+++ src/components/profiles/vamsi.jsx
@@ -40,7 +40,7 @@ const profile11 = () => {
               <li>Email	 (<i><FaEnvelope/></i>)	:  kvvamsi@iiti.ac.in </li>
               <li>Contact  (<i><FaPhone/></i>)		: +91-7316603333</li>
             <div className="fac-links">
-            <a href="https://kvvamsi20.wixsite.com/vamsi" target="_blank" rel="noreferrer"> <i><FaHome/></i></a>
+            <a href="https://kvvamsi.profiles.iiti.ac.in/" target="_blank" rel="noreferrer"> <i><FaHome/></i></a>
              <a href=" https://scholar.google.co.in/citations?user=ducGUjAAAAAJ&hl=en  "target="_blank"  rel="noreferrer"> <i><SiGooglescholar/></i></a>
              <a href="https://www.researchgate.net/profile/K-V-Vamsi "target="_blank"  rel="noreferrer"><i><FaResearchgate/></i></a>
              <a href="https://twitter.com/kvvamsi "target="_blank"  rel="noreferrer"><i><FaTwitterSquare/></i></a>
